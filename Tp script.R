62cc82c20912bb2f6904f5819e6eb30f5ead443d
GuidoGabrielePeruilh <58449852+GuidoGabrielePeruilh@users.noreply.github.com>
2019-12-07 17:06:51 -0300

    Add files via upload

--- Tp script.R
+++ Tp script.R
@@ -19,10 +19,6 @@ library(psych)
 describe(Data,quant = c(0,0.25,0.5,0.75,1)) #Analisis descriptivo
 #skew es la asmimetria. Se encuentran cercanos a 0
 
-#Analizamos la Kurtosis de ambas series
-
-kurtosi(Data) #Ambas son planicurticas
-
 #Graficamos el Boxplot
 par(mfrow = c(1,2))
 boxplot(Serie_A,main = "Box Plot Serie A Grupo 13", col = "red")
@@ -53,31 +49,5 @@ acf(Serie_B_Diff, main = "FAC Serie B Dif", col = "green")
 pacf(Serie_A, main = "FACP Serie A", col = "red") 
 pacf(Serie_B_Diff, main = "FACP Serie B Dif", col = "green") #Ahora si parece ser estacionario  
 
-#Modelizamos ---> Primero hay que testear
-M1_SerieA<-arima(Serie_A,order = c(1,0,0))
-M1_SerieA
-
-M2_SerieA<-arima(Serie_A,order = c(0,0,2))
-M2_SerieA
-
-M3_SerieA<-arima(Serie_A,order = c(1,0,1))
-M3_SerieA
-
-M4_SerieA<-arima(Serie_A,order=c(2,0,1))
-M4_SerieA
-
-M5_SerieA<-arima(Serie_A,order=c(2,0,2))
-M5_SerieA
-
-M6_SerieA<-arima(Serie_A,order=c(1,1,1))
-M6_SerieA
-
-M7_SerieA<-arima(Serie_A,order=c(2,1,0))
-M7_SerieA
-
-M8_SerieA<-arima(Serie_A,order=c(1,2,2))
-M8_SerieA
 
 
-M1_SerieB<-arima(Serie_B_Diff,c(1,1,1))
-M1_SerieB #Hay raiz unitaria, por lo tanto no es estacionario
